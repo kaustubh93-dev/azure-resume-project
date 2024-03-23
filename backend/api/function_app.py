f893a29f957f314dd2e42a72d857575c69456a4b
kaustubh-93 <55542518+kaustubh93-dev@users.noreply.github.com>
2024-03-23 23:43:49 +0530

    function_app.py

--- backend/api/function_app.py
+++ backend/api/function_app.py
@@ -1,25 +1,30 @@
+import os
 import azure.functions as func
+from azure.cosmos import CosmosClient, exceptions
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+app = func.FunctionApp(__name__, level=func.AuthLevel.FUNCTION)
 
-@app.route(route="GetResumeCounter")
-def GetResumeCounter(req: func.HttpRequest) -> func.HttpResponse:
+CONNECTION_STRING = os.environ['AzureCosmosDBConnectionString']
+COSMOS_CLIENT = CosmosClient.from_connection_string(CONNECTION_STRING)
+DATABASE = COSMOS_CLIENT.get_database_client('AzureResume')
+CONTAINER = DATABASE.get_container_client('Counter')
+
+@app.route(router_map='trigger')
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    count = -1
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
+        query = "SELECT * FROM c WHERE c.id='a'"
+        for item in CONTAINER.query_items(query=query, enable_cross_partition_query=True):
+            updated_item = item
+            count += 1
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        updated_item['count'] += str(count)
+        CONTAINER.upsert_item(updated_item)
+
+    except exceptions.CosmosHttpResponseError as e:
+        logging.error("ERROR occurred: {status_code} - {message}".format(status_code=e.status_code, message=e.message))
+    
+    return func.HttpResponse(body=str(count), status_code=200)
