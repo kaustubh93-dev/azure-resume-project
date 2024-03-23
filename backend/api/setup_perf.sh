0175e49045d18eb543d636d2051c4156749c1157
kaustubh-93 <55542518+kaustubh93-dev@users.noreply.github.com>
2024-03-23 17:36:07 +0530

    api

--- backend/api/setup_perf.sh
+++
@@ -1,4 +0,0 @@
-az functionapp config appsettings set -g cloud-resume-challenge-rg -n AzureResumeVisitorCounter --settings 'WEBSITE_USE_PLACEHOLDER_DOTNETISOLATED=1'
-az functionapp config set -g cloud-resume-challenge-rg -n AzureResumeVisitorCounter --net-framework-version 
-"v8.0"
-az functionapp config set -g cloud-resume-challenge-rg -n AzureResumeVisitorCounter --use-32bit-worker-process false
